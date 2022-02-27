4de0ab73a3de3551d96ed7b0a589584f213ef51a
Igor Stepanov <igorstepanov@hotmail.com>
2022-02-27 14:20:36 +0300

    build(дипломная работа): cоздать схемы, модели ресурсов API и статусы ошибок

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const { PORT = 3000 } = process.env;
 const app = express();
+
+mongoose.connect('mongodb://localhost:27017/bitfilmsdb', () => {
+  console.log('Connection successful');
+});
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`)
---
+++ models/user.js
@@ -0,0 +1,47 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+const isEmail = require('validator/lib/isEmail');
+const AuthError = require('../errors/AuthError');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    // default: 'Коломбо',
+    required: true,
+    minlength: 2,
+    maxlength: 30,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: {
+      validator: (email) => isEmail(email),
+      message: 'Некорректый адрес почты',
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+    select: false,
+  },
+});
+
+// eslint-disable-next-line func-names
+userSchema.statics.findUserByCredentials = function (email, password) {
+  return this.findOne({ email }).select('+password')
+    .then((user) => {
+      if (!user) {
+        return Promise.reject(new AuthError('Неправильная почта или пароль'));
+      }
+      return bcrypt.compare(password, user.password)
+        .then((matched) => {
+          if (!matched) {
+            return Promise.reject(new AuthError('Неправильная почта или пароль'));
+          }
+          return user;
+        });
+    });
+};
+
+module.exports = mongoose.model('user', userSchema);
