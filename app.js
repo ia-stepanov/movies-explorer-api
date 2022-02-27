1b1725b853e9a25d3434aa2fed886c9ee9cae5e5
Igor Stepanov <igorstepanov@hotmail.com>
2022-02-27 19:40:50 +0300

    build(дипломная работа): реализовать логирование

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const { errors } = require('celebrate');
 const errorHandler = require('./middlewares/errorHandler');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 const routes = require('./routes');
 
 const { PORT = 3000 } = process.env;
@@ -12,7 +14,19 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(routes);
+app.use(cors);
+
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
+
+app.use(requestLogger);
+
+app.use('/', routes);
+
+app.use(errorLogger);
 
 app.use(errors());
 app.use(errorHandler);
