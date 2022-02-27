39e84a704dc7ed8b2a0ba9bac111237c3ea4a068
Igor Stepanov <igorstepanov@hotmail.com>
2022-02-27 18:51:29 +0300

    build(дипломная работа): создать валидацию

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const { errors } = require('celebrate');
+const errorHandler = require('./middlewares/errorHandler');
 
 const { PORT = 3000 } = process.env;
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(errors());
+app.use(errorHandler);
 
 mongoose.connect('mongodb://localhost:27017/bitfilmsdb', () => {
   console.log('Connection successful');
---
+++ middlewares/validations.js
@@ -0,0 +1,53 @@
+const { celebrate, Joi } = require('celebrate');
+const isUrl = require('validator/lib/isURL');
+const BadRequest = require('../errors/BadRequest');
+
+const validationUrl = (url) => {
+  const validate = isUrl(url);
+  if (validate) {
+    return url;
+  }
+  throw new BadRequest('Некорректный адрес URL');
+};
+
+const validationGetUser = celebrate({
+  params: Joi.object().keys({
+    userId: Joi.string().alphanum().length(24).hex(),
+  }),
+});
+
+const validationUpdateUser = celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().required().min(2).max(30),
+    email: Joi.string().required().email(),
+  }),
+});
+
+const validationCreateMovie = celebrate({
+  body: Joi.object().keys({
+    country: Joi.string().required(),
+    director: Joi.string().required(),
+    duration: Joi.number().required(),
+    year: Joi.string().required(),
+    description: Joi.string().required(),
+    image: Joi.string().required().custom(validationUrl),
+    trailerLink: Joi.string().required().custom(validationUrl),
+    thumbnail: Joi.string().required().custom(validationUrl),
+    movieId: Joi.number().required(),
+    nameRU: Joi.string().required(),
+    nameEN: Joi.string().required(),
+  }),
+});
+
+const validationDeleteMovie = celebrate({
+  params: Joi.object().keys({
+    movieId: Joi.string().required().hex().length(24),
+  }),
+});
+
+module.exports = {
+  validationGetUser,
+  validationUpdateUser,
+  validationCreateMovie,
+  validationDeleteMovie,
+};
