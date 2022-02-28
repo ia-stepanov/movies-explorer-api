250d0ebef9abbcdccf6e194dc261fd4fca320868
Igor Stepanov <igorstepanov@hotmail.com>
2022-02-28 23:09:17 +0300

    fix(дипломная работа): исправить cors (к серверу можно обратиться с помощью https)

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(cors);
+app.use(cors());
 
 app.get('/crash-test', () => {
   setTimeout(() => {
